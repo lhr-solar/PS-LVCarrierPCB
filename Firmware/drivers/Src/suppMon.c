b4a09128a4565f99c5b05cc22ea714f5d7df6913
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-02-09 23:45:59 -0600

    shdn testing on the board

--- Firmware/drivers/Src/suppMon.c
+++ Firmware/drivers/Src/suppMon.c
@@ -8,6 +8,23 @@ static uint8_t adc1_static_storage[ADC1_QUEUE_LENGTH * ADC_ITEM_SIZE];
 static StaticQueue_t adc1QueueBuffer;
 static QueueHandle_t adc1RecvQ;
 
+#define ADC2_QUEUE_LENGTH   10
+
+static uint8_t adc2_static_storage[ADC1_QUEUE_LENGTH * ADC_ITEM_SIZE];
+static StaticQueue_t adc2QueueBuffer;
+static QueueHandle_t adc2RecvQ;
+
+
+ADC_InitTypeDef adc_init_1;
+
+static adc_status_t adc1_init(){
+  return ADC_OK;
+
+}
+
+static adc_status_t adc2_init(){
+  return ADC_OK;
+}
 
 adc_status_t lv_carrier_adc_init(void){
     adc1RecvQ = xQueueCreateStatic( ADC1_QUEUE_LENGTH, ADC_ITEM_SIZE, adc1_static_storage, &adc1QueueBuffer);
@@ -16,10 +33,21 @@ adc_status_t lv_carrier_adc_init(void){
         return ADC_INIT_FAIL;
     }
 
+    adc2RecvQ = xQueueCreateStatic( ADC2_QUEUE_LENGTH, ADC_ITEM_SIZE, adc2_static_storage, &adc2QueueBuffer);
+
+    if (adc2RecvQ == NULL) {
+        return ADC_INIT_FAIL;
+    }
+
+    adc1_init();
+    adc2_init();
+
     return ADC_OK;
 
 }
 
+
+static uint32_t HAL_RCC_ADC12_CLK_ENABLED=0;
 
 void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
 {
@@ -28,6 +56,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
   if(adcHandle->Instance==ADC1)
   {
+
   /** Initializes the peripherals clocks
   */
     PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC12;
@@ -38,12 +67,22 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
     }
 
     /* ADC1 clock enable */
-    __HAL_RCC_ADC12_CLK_ENABLE();
+    HAL_RCC_ADC12_CLK_ENABLED++;
+    if(HAL_RCC_ADC12_CLK_ENABLED==1){
+      __HAL_RCC_ADC12_CLK_ENABLE();
+    }
 
+    __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
     /**ADC1 GPIO Configuration
+    PA2     ------> ADC1_IN3
     PB0     ------> ADC1_IN15
     */
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_0;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -53,20 +92,81 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
     HAL_NVIC_SetPriority(ADC1_2_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(ADC1_2_IRQn);
   }
+  else if(adcHandle->Instance==ADC2)
+  {
+
+  /** Initializes the peripherals clocks
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC12;
+    PeriphClkInit.Adc12ClockSelection = RCC_ADC12CLKSOURCE_SYSCLK;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* ADC2 clock enable */
+    HAL_RCC_ADC12_CLK_ENABLED++;
+    if(HAL_RCC_ADC12_CLK_ENABLED==1){
+      __HAL_RCC_ADC12_CLK_ENABLE();
+    }
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**ADC2 GPIO Configuration
+    PA1     ------> ADC2_IN2
+    PB2     ------> ADC2_IN12
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* ADC2 interrupt Init */
+    HAL_NVIC_SetPriority(ADC1_2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(ADC1_2_IRQn);
+  }
 }
 
 void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
 {
+
   if(adcHandle->Instance==ADC1)
   {
-    __HAL_RCC_ADC12_CLK_DISABLE();
+
+    /* Peripheral clock disable */
+    HAL_RCC_ADC12_CLK_ENABLED--;
+    if(HAL_RCC_ADC12_CLK_ENABLED==0){
+      __HAL_RCC_ADC12_CLK_DISABLE();
+    }
 
     /**ADC1 GPIO Configuration
+    PA2     ------> ADC1_IN3
     PB0     ------> ADC1_IN15
     */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2);
+
     HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0);
 
-    HAL_NVIC_DisableIRQ(ADC1_2_IRQn);
+  }
+  else if(adcHandle->Instance==ADC2)
+  {
+    /* Peripheral clock disable */
+    HAL_RCC_ADC12_CLK_ENABLED--;
+    if(HAL_RCC_ADC12_CLK_ENABLED==0){
+      __HAL_RCC_ADC12_CLK_DISABLE();
+    }
+
+    /**ADC2 GPIO Configuration
+    PA1     ------> ADC2_IN2
+    PB2     ------> ADC2_IN12
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_2);
   }
 }
-
---
+++ Firmware/tests/shdn_test.c
@@ -0,0 +1,52 @@
+#include "stm32xx_hal.h"
+#include "pinDefs.h"
+
+void Clock_Init(uint32_t port){
+    switch (port) {
+        case (uint32_t)GPIOA:
+            __HAL_RCC_GPIOA_CLK_ENABLE();
+            break;
+        case (uint32_t)GPIOB:
+            __HAL_RCC_GPIOB_CLK_ENABLE();
+            break;
+        case (uint32_t)GPIOC:
+            __HAL_RCC_GPIOC_CLK_ENABLE();
+            break;
+        default:
+            break;
+    }
+}
+
+
+int main(){
+    HAL_Init();
+
+    GPIO_InitTypeDef led_config = {
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Pin = LTC4421_SHDN_PIN
+    };
+    
+    Clock_Init((uint32_t)LTC4421_SHDN_PORT);
+    HAL_GPIO_Init(LTC4421_SHDN_PORT, &led_config); // initialize HB_LED_PORT with led_config
+
+    GPIO_InitTypeDef hb_led_config = {
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Pin = HEARTBEAT_LED_PIN
+    };
+    
+    Clock_Init((uint32_t)HEARTBEAT_LED_PORT);
+    HAL_GPIO_Init(HEARTBEAT_LED_PORT, &hb_led_config); // initialize HB_LED_PORT with led_config
+    
+    HAL_GPIO_WritePin(LTC4421_SHDN_PORT, LTC4421_SHDN_PIN, GPIO_PIN_RESET);
+    HAL_Delay(5000);
+    HAL_GPIO_WritePin(LTC4421_SHDN_PORT, LTC4421_SHDN_PIN, GPIO_PIN_SET);
+
+    while(1){
+        HAL_GPIO_TogglePin(HEARTBEAT_LED_PORT, HEARTBEAT_LED_PIN);
+        HAL_Delay(5000);
+    }
+
+    return 0;
+}
