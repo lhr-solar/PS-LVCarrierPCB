52d9cca4762b29d92f6933e2d1b3a8bb627894ad
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-02-13 17:44:07 -0600

    LV enable

--- Firmware/drivers/Src/lvEnable.c
+++ Firmware/drivers/Src/lvEnable.c
@@ -6,11 +6,11 @@ void lvEnable_gpio_init(void){
 }
 
 pin_state_t get_lvEnable_supp(void){
-    // lv enable supp is negative logic
-    return HAL_GPIO_ReadPin(LV_EN_SUPP_PORT, LV_EN_SUPP_PIN) == GPIO_PIN_SET ? OFF : ON;
+    // lv enable supp is positive logic
+    return HAL_GPIO_ReadPin(LV_EN_SUPP_PORT, LV_EN_SUPP_PIN) == GPIO_PIN_SET ? ON : OFF;
 }
 
 pin_state_t get_lvEnable_powerSupply(void){
-    // lv enable power supply is negative logic
-    return HAL_GPIO_ReadPin(LV_EN_POWERSUPPLY_PORT, LV_EN_POWERSUPPLY_PIN) == GPIO_PIN_SET ? OFF : ON;
+    // lv enable power supply is positive logic
+    return HAL_GPIO_ReadPin(LV_EN_POWERSUPPLY_PORT, LV_EN_POWERSUPPLY_PIN) == GPIO_PIN_SET ? ON : OFF;
 }
