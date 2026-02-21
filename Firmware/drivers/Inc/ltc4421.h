559b4e84152f209e6045efb2515188059e4d332c
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-02-21 11:17:52 -0600

    ltc testing

--- Firmware/drivers/Inc/ltc4421.h
+++ Firmware/drivers/Inc/ltc4421.h
@@ -7,7 +7,21 @@
 #define LTC4421_SHUTDOWN OFF
 #define LTC4421_ENABLED ON
 
+#define LTC4421_HV_DCDC_CHANNEL CHANNEL1
+#define LTC4421_SUPP_DCDC_CHANNEL CHANNEL2
+
+typedef enum {
+    NO_CHANNEL,
+    CHANNEL1,
+    CHANNEL2
+}ltc4421_channel_t;
 
 void ltc4421_gpio_init(void);
 
 void ltc4421_shdn_enable(pin_state_t newShutDownState);
+
+pin_state_t ltc4421_read_valid(ltc4421_channel_t channel);
+
+ltc4421_channel_t ltc4421_channel_selected(void);
+
+pin_state_t ltc4421_read_fault(ltc4421_channel_t channel);
